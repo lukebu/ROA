0ca36b794b2f3698bee5dd426177df32620498b0
NBLBUDA <lukasz.p.buda@comarch.pl>
2019-05-04 01:38:19 +0200

    contact list

--- src/main/java/com/lukebu/workmt/contact/Contact.java
+++ src/main/java/com/lukebu/workmt/contact/Contact.java
@@ -1,35 +1,88 @@
 package com.lukebu.workmt.contact;
 
+import javafx.beans.property.SimpleStringProperty;
+
 public class Contact {
+    public int getUsrId() {
+        return usrId;
+    }
+
+    public String getContactName() {
+        return contactName.get();
+    }
+
+    public SimpleStringProperty contactNameProperty() {
+        return contactName;
+    }
+
+    public String getContactSurname() {
+        return contactSurname.get();
+    }
+
+    public SimpleStringProperty contactSurnameProperty() {
+        return contactSurname;
+    }
+
+    public String getContactMobilePhone() {
+        return contactMobilePhone.get();
+    }
+
+    public SimpleStringProperty contactMobilePhoneProperty() {
+        return contactMobilePhone;
+    }
+
+    public String getContactDesktopPhone() {
+        return contactDesktopPhone.get();
+    }
+
+    public SimpleStringProperty contactDesktopPhoneProperty() {
+        return contactDesktopPhone;
+    }
+
+    public String getContactEmail() {
+        return contactEmail.get();
+    }
+
+    public SimpleStringProperty contactEmailProperty() {
+        return contactEmail;
+    }
+
+    public String getContactType() {
+        return contactType.get();
+    }
+
+    public SimpleStringProperty contactTypeProperty() {
+        return contactType;
+    }
 
     private int contactId;
     private int usrId;
-    private String contactName;
-    private String contactSurname;
-    private String contactMobilePhone;
-    private String contactDesktopPhone;
-    private String contactEmail;
-    private String contactType;
+    private SimpleStringProperty contactName;
+    private SimpleStringProperty contactSurname;
+    private SimpleStringProperty contactMobilePhone;
+    private SimpleStringProperty contactDesktopPhone;
+    private SimpleStringProperty contactEmail;
+    private SimpleStringProperty contactType;
 
     public Contact(int usrId, String contactName, String contactSurname, String contactMobilePhone, String contactDesktopPhone, String contactEmail, String contactType) {
         this.usrId = usrId;
-        this.contactName = contactName;
-        this.contactSurname = contactSurname;
-        this.contactMobilePhone = contactMobilePhone;
-        this.contactDesktopPhone = contactDesktopPhone;
-        this.contactEmail = contactEmail;
-        this.contactType = contactType;
+        this.contactName = new SimpleStringProperty(contactName);
+        this.contactSurname = new SimpleStringProperty(contactSurname);
+        this.contactMobilePhone = new SimpleStringProperty(contactMobilePhone);
+        this.contactDesktopPhone = new SimpleStringProperty(contactDesktopPhone);
+        this.contactEmail = new SimpleStringProperty(contactEmail);
+        this.contactType = new SimpleStringProperty(contactType);
     }
 
     public Contact(int contactId, int usrId, String contactName, String contactSurname, String contactMobilePhone, String contactDesktopPhone, String contactEmail, String contactType) {
         this.contactId = contactId;
         this.usrId = usrId;
-        this.contactName = contactName;
-        this.contactSurname = contactSurname;
-        this.contactMobilePhone = contactMobilePhone;
-        this.contactDesktopPhone = contactDesktopPhone;
-        this.contactEmail = contactEmail;
-        this.contactType = contactType;
+        this.contactName = new SimpleStringProperty(contactName);
+        this.contactSurname = new SimpleStringProperty(contactSurname);
+        this.contactMobilePhone = new SimpleStringProperty(contactMobilePhone);
+        this.contactDesktopPhone = new SimpleStringProperty(contactDesktopPhone);
+        this.contactEmail = new SimpleStringProperty(contactEmail);
+        this.contactType = new SimpleStringProperty(contactType);
     }
 
     public int getContactId() {
--- src/main/java/com/lukebu/workmt/contact/ContactListController.java
+++ src/main/java/com/lukebu/workmt/contact/ContactListController.java
@@ -1,19 +1,15 @@
 package com.lukebu.workmt.contact;
 
-import com.lukebu.workmt.ChangeSceneProcessor;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
-import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
@@ -23,19 +19,19 @@ public class ContactListController implements Initializable {
     private ObservableList<Contact> contacts = FXCollections.observableArrayList();
 
     @FXML
-    private TableView tableView;
+    private TableView<Contact> tableView = new TableView<Contact>();
     @FXML
-    private TableColumn contactName;
+    private TableColumn<Contact, String> contactName;
     @FXML
-    private TableColumn contactSurname;
+    private TableColumn<Contact, String> contactSurname;
     @FXML
-    private TableColumn mobilePhone;
+    private TableColumn<Contact, String> mobilePhone;
     @FXML
-    private TableColumn desktopPhone;
+    private TableColumn<Contact, String> desktopPhone;
     @FXML
-    private TableColumn email;
+    private TableColumn<Contact, String> email;
     @FXML
-    private TableColumn contactType;
+    private TableColumn<Contact, String> contactType;
     @FXML
     private BorderPane rootPane;
 
@@ -47,31 +43,37 @@ public class ContactListController implements Initializable {
             contactDataProcessing.loadContactListFormDB();
             contacts = ContactData.getInstance().getContactsList();
             //findListChange();
-            tableView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-            tableView.getSelectionModel().selectFirst();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        refreshView();
     }
 
-    /*private void refreshView() {
-        tableView.getSelectionModel().selectLast();
-    }
+    private void refreshView() {
+        contactName.setCellValueFactory(new PropertyValueFactory<>("contactName"));
+        contactSurname.setCellValueFactory(new PropertyValueFactory<>("contactSurname"));
+        mobilePhone.setCellValueFactory(new PropertyValueFactory<>("contactMobilePhone"));
+        desktopPhone.setCellValueFactory(new PropertyValueFactory<>("contactDesktopPhone"));
+        email.setCellValueFactory(new PropertyValueFactory<>("contactEmail"));
+        contactType.setCellValueFactory(new PropertyValueFactory<>("contactType"));
 
-    private void findListChange() {
-        tableView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Contact>() {
-            @Override
-            public void changed(ObservableValue<? extends Contact> observable, Contact oldValue, Contact newValue) {
-                if (newValue != null) {
+        tableView.setItems(contacts);
+        tableView.getItems();
 
-                }
-            };
-        });
-    }*/
-
-    @FXML
-    private void handleModifyTaskOnList(ActionEvent event) {
-        ChangeSceneProcessor.changeScene(getClass().getResource("/scenes/task/modifyTask.fxml"), "Zmodyfikuj zadanie", null);
+//    private void findListChange() {
+//        tableView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Contact>() {
+//            @Override
+//            public void changed(ObservableValue<? extends Contact> observable, Contact oldValue, Contact newValue) {
+//                if (newValue != null) {
+//
+//                }
+//            };
+//        });
+//    }*/
+//
+//    @FXML
+//    private void handleModifyTaskOnList(ActionEvent event) {
+//        ChangeSceneProcessor.changeScene(getClass().getResource("/scenes/task/modifyTask.fxml"), "Zmodyfikuj zadanie", null);
 
         /*Contact contact = tableView.getSelectionModel().getSelectedItem();
         ModifyTaskEvent modifyTaskEvent = new ModifyTaskEvent();
